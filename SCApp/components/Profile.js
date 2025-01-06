06c4e437110b1e49b5108bd302bf23365375c30f
Pabodha_Wijesinghe <139679251+kushipabo@users.noreply.github.com>
2025-01-06 11:38:11 +0000

    update profile file

--- SCApp/components/Profile.js
+++ SCApp/components/Profile.js
@@ -1,67 +1,182 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { Avatar, Card } from "react-native-paper";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+import {
+  Button,
+  Divider,
+  PaperProvider,
+  Text,
+  TextInput,
+} from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
-const Profile = ({ student }) => {
+export default function Profile({ route }) {
+  const { studentdata } = route.params;
+  const navigate = useNavigation();
   return (
-    <View style={styles.view}>
-      <Image source={require("../assets/logo.png")} style={styles.image} />
-
-      <Card style={{ margin: 20 }}>
-        <Card.Content style={styles.cardContent}>
-          <Image source={student.profile_pic} style={styles.profile_pic} />
-          <Text style={styles.h1}>{student.name}</Text>
-          <Text style={{ textAlign: "center" }}>
-            Age : {student.age} | Gender: {student.gender}
-          </Text>
-
-          <Text style={{ fontWeight: "bold", marginTop: 20 }}>
-            Contact Information
-          </Text>
-          <Text>Email: {student.email}</Text>
-          <Text>Phone: {student.phone}</Text>
-          <Text>Address: {student.address}</Text>
-
-          <View
-            style={{
-              marginBlock: 20,
-              borderBottomColor: "black",
-              borderBottomWidth: StyleSheet.hairlineWidth,
-            }}
+    <PaperProvider>
+      <ScrollView>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+        <View style={styles.imagecontain}>
+          <Image
+            source={require("../assets/uovlogo.png")}
+            style={styles.image}
           />
+        </View>
 
-          <Text style={{ fontWeight: "bold" }}>Biological Information</Text>
-          <Text>Gender: {student.gender}</Text>
-          <Text>Age: {student.age}</Text>
-          <Text>Blood Group: {student.blood_group}</Text>
-        </Card.Content>
-      </Card>
-    </View>
+        <View style={styles.studentInfoContainer}>
+          <Image
+            style={styles.profilePicContainer}
+            source={studentdata.profile_pic}
+          ></Image>
+          <Text style={styles.studentName}>Name:{studentdata.username}</Text>
+          <Text style={styles.studentDetails}>Age:{studentdata.age}</Text>
+          <Text style={styles.studentDetails}>Gender:{studentdata.gender}</Text>
+        </View>
+
+        <View style={styles.contactInfoContainer}>
+          <Text style={styles.contactTitle}>Contact Information</Text>
+          <Text style={styles.contactDetails}>Email: {studentdata.email}</Text>
+          <Text style={styles.contactDetails}>Phone: {studentdata.phone}</Text>
+          <Text style={styles.contactDetails}>
+            Address: {studentdata.address}
+          </Text>
+        </View>
+
+        <Divider></Divider>
+        <View style={styles.biologicalInfoContainer}>
+          <Text style={styles.biologicalTitle}>Biological Information</Text>
+          <Text style={styles.biologicalDetails}>
+            Gender: {studentdata.gender}
+          </Text>
+          <Text style={styles.biologicalDetails}>Age: {studentdata.age}</Text>
+          <Text style={styles.biologicalDetails}>
+            Blood Group: {studentdata.blood_group}
+          </Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            mode="contained"
+            onPress={() => {
+              navigate.navigate("profile", { studentdata });
+            }}
+            style={styles.button}
+          >
+            Profile
+          </Button>
+          <Button
+            mode="contained"
+            onPress={() => {
+              navigate.navigate("course");
+            }}
+            style={styles.button}
+          >
+            Course
+          </Button>
+          <Button
+            mode="contained"
+            onPress={() => {
+              navigate.navigate("subjects");
+            }}
+            style={styles.button}
+          >
+            Subjects
+          </Button>
+        </View>
+      </ScrollView>
+    </PaperProvider>
   );
-};
-
-export default Profile;
+}
 
 const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-  },
-  h1: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 4,
-    textAlign: "center",
-  },
-  image: {
-    marginTop: 20,
+  header: {
     width: "100%",
     height: 100,
-    resizeMode: "contain",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#8b008b",
   },
-  profile_pic: {
-    width: 150,
-    height: 150,
-    borderRadius: 100,
-    margin: "auto",
+  headerText: {
+    fontSize: 24,
+    color: "#fff",
+    backgroundColor: "#8b008b",
+    fontWeight: "bold",
+  },
+  image: {
+    width: "60%",
+    height: 100,
+    resizeMode: "contain",
+    marginTop: 5,
+  },
+  imagecontain: { alignItems: "center" },
+  loginText: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  studentInfoContainer: {
+    width: "100%",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  profilePicContainer: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    overflow: "hidden",
+    marginBottom: 20,
+    borderWidth: 3,
+    borderColor: "#8b008b", // Matching border color
+  },
+  studentDetails: {
+    fontSize: 18,
+    color: "#555",
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  studentName: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10,
+  },
+  contactInfoContainer: {
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+  contactTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#8b008b",
+  },
+  contactDetails: {
+    fontSize: 10,
+    color: "#555",
+    marginBottom: 5,
+  },
+
+  biologicalInfoContainer: {
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+  biologicalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#8b008b",
+  },
+  biologicalDetails: {
+    fontSize: 10,
+    color: "#555",
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  button: {
+    marginHorizontal: 5,
   },
 });
